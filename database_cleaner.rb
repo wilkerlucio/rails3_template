1b62a8226fe1ee6eeb24686ff7486f1443988f0f
Wilker Lucio <wilkerlucio@gmail.com>
2010-09-14 21:20:30 -0300

    more rspec and database cleaner configuration

--- database_cleaner.rb
+++ database_cleaner.rb
@@ -4,8 +4,14 @@ gem 'database_cleaner', '0.5.2', :group => :test
 @delayed << lambda {
   # setup database cleaner strategy for rspec
   if @use_rspec
-    append_file "spec/spec_helper.rb" do
-      "DatabaseCleaner.strategy = :truncation"
+    # configure filters
+    inject_into_file "spec/spec_helper.rb", :after => "config.mock_with :rspec\n" do
+      "\n  config.before(:suite) do\n" +
+      "    DatabaseCleaner.strategy = :truncation\n" +
+      "  end\n\n" +
+      "  config.after(:each) do\n" +
+      "    DatabaseCleaner.clean\n" +
+      "  end\n\n"
     end
   end
 }
--- rspec.rb
+++ rspec.rb
@@ -12,6 +12,14 @@ gem 'rspec-rails', '2.0.0.beta.20', :group => :test
   # generate
   generate "rspec:install"
   
+  # configure filters
+  inject_into_file "spec/spec_helper.rb", :after => "config.mock_with :rspec\n" do
+    "  config.filter_run :focus => true\n" +
+    "  config.run_all_when_everything_filtered = true\n"
+  end
+  
+  gsub_file "spec/spec_helper.rb", %{config.use_transactional_fixtures = true}, %{config.use_transactional_fixtures = false}
+  
   # rspec runner configuration
   get "#{File.dirname(__FILE__)}/resources/dot_rspec", ".rspec", :force => true
 }
