010b6e546542cec12719fae627b3ab8a6441d516
Wilker Lucio <wilkerlucio@gmail.com>
2010-09-15 21:40:02 -0300

    custom _form for simple_form

--- simple_form.rb
+++ simple_form.rb
@@ -3,4 +3,7 @@ gem 'simple_form', '1.2.2'
 
 @delayed << lambda {
   generate "simple_form:install"
+  
+  get "#{File.dirname(__FILE__)}/resources/_form.html.erb", "lib/templates/erb/scaffold/_form.html.erb", :force => true
+  get "#{File.dirname(__FILE__)}/resources/_form_errors.html.erb", "app/views/layouts/_form_errors.html.erb"
 }
